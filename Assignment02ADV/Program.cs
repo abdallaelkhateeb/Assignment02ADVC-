2c67ab1963726e32dce9af723f6e3637c6ea8280
abdallaelkhateeb <119896180+abdallaelkhateeb@users.noreply.github.com>
2025-07-31 10:45:03 +0300

    Add project files.

---
+++ Assignment02ADV/Program.cs
@@ -0,0 +1,133 @@
+﻿namespace Assignment02ADV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+
+            //string[] input = Console.ReadLine().Split();
+            //int n = int.Parse(input[0]);
+            //int q = int.Parse(input[1]);
+
+            //int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            //for (int i = 0; i < q; i++)
+            //{
+            //    int x = int.Parse(Console.ReadLine());
+            //    int count = 0;
+
+            //    foreach (int num in arr)
+            //    {
+            //        if (num > x)
+            //            count++;
+            //    }
+            //    Console.WriteLine($"count :{count}");
+
+            //}
+            #endregion
+
+            #region Q2
+
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //bool isPalindrome = true;
+
+            //for (int i = 0; i < n / 2; i++)
+            //{
+            //    if (arr[i] != arr[n - i - 1])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine(isPalindrome ? "YES" : "NO");
+
+            #endregion
+
+            #region Q3
+
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+
+            //Stack<int> stack = new Stack<int>();
+            //while (queue.Count > 0)
+            //    stack.Push(queue.Dequeue());
+
+            //while (stack.Count > 0)
+            //    queue.Enqueue(stack.Pop());
+
+            //foreach (var item in queue)
+            //    Console.Write(item + " ");
+            #endregion
+
+
+            #region Q4
+            //string input = Console.ReadLine();
+            //Stack<char> stack = new Stack<char>();
+
+            //bool balanced = true;
+            //foreach (char c in input)
+            //{
+            //    if ("({[".Contains(c))
+            //        stack.Push(c);
+            //    else if (")}]".Contains(c))
+            //    {
+            //        if (stack.Count == 0 ||
+            //            (c == ')' && stack.Pop() != '(') ||
+            //            (c == ']' && stack.Pop() != '[') ||
+            //            (c == '}' && stack.Pop() != '{'))
+            //        {
+            //            balanced = false;
+            //            break;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine(balanced && stack.Count == 0 ? "Balanced" : "Not Balanced");
+
+
+            #endregion
+
+
+            #region Q5
+
+            #endregion
+
+            #region Q6
+
+            #endregion
+
+            #region Q7
+
+            #endregion
+
+            #region Q8
+
+            #endregion
+
+            #region Q9
+
+            #endregion
+
+            #region Q10
+
+            #endregion
+
+            #region Q11
+
+            #endregion
+
+
+
+
+
+        }
+    }
+
+}
+
